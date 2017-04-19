adb1db9588e872382365962b4465bcc448122c7e
KlasafGeijerstam <klasafgeijerstam@gmail.com>
2017-04-20 00:38:51 +0200

    Added old solutions

---
+++ KatFetch/KatFetch/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace KatFetch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.wb = new System.Windows.Forms.WebBrowser();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.wb2 = new System.Windows.Forms.WebBrowser();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.wb);
+            this.groupBox1.Location = new System.Drawing.Point(12, 37);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(441, 463);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // wb
+            // 
+            this.wb.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.wb.Location = new System.Drawing.Point(3, 16);
+            this.wb.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wb.Name = "wb";
+            this.wb.Size = new System.Drawing.Size(435, 444);
+            this.wb.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(227, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Go!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 11);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(121, 11);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(308, 8);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(236, 23);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.wb2);
+            this.groupBox2.Location = new System.Drawing.Point(409, 37);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(441, 463);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            // 
+            // wb2
+            // 
+            this.wb2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.wb2.Location = new System.Drawing.Point(3, 16);
+            this.wb2.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wb2.Name = "wb2";
+            this.wb2.Size = new System.Drawing.Size(435, 444);
+            this.wb2.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(550, 8);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Max facebook";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(755, 512);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.WebBrowser wb;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.WebBrowser wb2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ KatFetch/KatFetch/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KatFetch
+{
+    public partial class Form1 : Form
+    {
+        private int stage = 0;
+        private int index = 0;
+        private int index2 = 0;
+        private List<Solution> solutions = new List<Solution>();
+        private List<Solution> solutions2;
+        private WebClient wc = new WebClient();
+        private WebClient wc2 = new WebClient();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            wb.Navigate("https://open.kattis.com/login/");
+        }
+
+        private void HandleMup(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if(stage == 0)
+            {
+                wb.Document.GetElementById("user_input").SetAttribute("value", textBox1.Text);
+                wb.Document.GetElementById("password_input").SetAttribute("value", textBox2.Text);
+                wb.Document.GetElementsByTagName("input")[4].InvokeMember("click");
+                stage++;
+            }
+            else if(stage == 1)
+            {
+                var p = wb.Document.GetElementsByTagName("a");
+                wb.Document.GetElementsByTagName("a")[8].InvokeMember("click");
+                stage++;
+            }
+            else if(stage == 2)
+            {
+                try
+                {
+                    var ar = wb.Document.GetElementsByTagName("tbody")[1].GetElementsByTagName("tr");
+
+                    foreach (HtmlElement i in ar)
+                    {
+                        if (i.Children[3].OuterText != "Accepted")
+                            continue;
+                        var s = new Solution();
+                        s.Url = i.Children[0].Children[0].GetAttribute("href");
+                        s.ID = i.Children[2].Children[0].GetAttribute("href").Split('/')[4];
+                        s.Language = i.Children[5].InnerText;
+                        if (!solutions.Contains(s))
+                        {
+                            Console.WriteLine(s);
+                            solutions.Add(s);
+                        }
+                    }
+                    var btn = wb.Document.GetElementById("problem_list_next");
+                    if (btn == null || btn.OuterHtml.Contains("disabled"))
+                    {
+                        progressBar1.Maximum = solutions.Count;
+                        progressBar1.Value = 0;
+                        solutions2 = solutions.Take(solutions.Count / 2).ToList();
+                        solutions = solutions.Skip(solutions.Count / 2).ToList();
+                        stage++;
+                        wb.Navigate(solutions[index].Url);
+                        wb2.Navigate(solutions2[index2].Url);
+                    }
+                    else
+                        btn.InvokeMember("click");
+                }
+                catch (Exception ass)
+                {
+                    Console.WriteLine(ass.StackTrace);
+                }
+            }
+            else if(stage == 3)
+            {
+                if (!Directory.Exists("./dl"))
+                    Directory.CreateDirectory("./dl");
+
+                if (index < solutions.Count)
+                {
+                    HtmlElement p = null;
+                    foreach (HtmlElement ep in wb.Document.GetElementsByTagName("a"))
+                    {
+                        if(ep.InnerText == "download")
+                        {
+                            p = ep;
+                            break;
+                        }
+                    }
+                    var url = p?.GetAttribute("href");
+                    var name = url.Split('/')[6].Split('.');
+
+                    wc.DownloadFile(url, "./dl/" + solutions[index].ID +"."+ name[1]);
+                    index++;
+                    progressBar1.Invoke(new Action(() => progressBar1.Increment(1)));
+                    wb.Navigate(solutions[index].Url);
+                }
+                else
+                {
+                    stage++;
+                    wb.Navigate("https://open.kattis.com/login/email");
+                }
+            }
+        }
+
+        class Solution : IEquatable<Solution>
+        {
+            public string Language { get; set; }
+            public string Url { get; set; }
+            public string ID { get; set; }
+
+            public bool Equals(Solution other)
+            {
+                return Language ==  other.Language && ID == other.ID;
+            }
+
+            public override string ToString()
+            {
+                return $"ID: {ID} Lang: {Language}";
+            }
+            public override bool Equals(object obj)
+            {
+                return Equals((Solution)obj);
+            }
+            public override int GetHashCode()
+            {
+                return Language.GetHashCode() + ID.GetHashCode();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            wb.DocumentCompleted += HandleMup;
+            wb.Navigate("https://open.kattis.com/login/email");
+            wb2.DocumentCompleted += Wb2_DocumentCompleted;
+            wb2.ScriptErrorsSuppressed = true;
+            wb.ScriptErrorsSuppressed = true;
+        }
+
+        private void Wb2_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (!Directory.Exists("./dl"))
+                Directory.CreateDirectory("./dl");
+
+            if (index < solutions.Count)
+            {
+                HtmlElement p = null;
+                foreach (HtmlElement ep in wb2.Document.GetElementsByTagName("a"))
+                {
+                    if (ep.InnerText == "download")
+                    {
+                        p = ep;
+                        break;
+                    }
+                }
+                var url = p?.GetAttribute("href");
+              
+                var name = url.Split('/')[6].Split('.');
+
+                wc2.DownloadFile(url, "./dl/" + solutions[index].ID + "." + name[1]);
+                index2++;
+                progressBar1.Invoke(new Action(() => progressBar1.Increment(1)));
+                wb2.Navigate(solutions2[index2].Url);
+            }
+            else
+            {
+                wb.Navigate("https://open.kattis.com/login/email");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            wb.DocumentCompleted += HandleMup;
+            stage = 1;
+            wb.Navigate("https://open.kattis.com/");
+            wb2.DocumentCompleted += Wb2_DocumentCompleted;
+            wb2.ScriptErrorsSuppressed = true;
+            wb.ScriptErrorsSuppressed = true;
+        }
+    }
+}
