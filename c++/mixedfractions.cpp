deab477eecc5fb1defc64ab4bdded8e16dc89e50
Klas af Geijerstam <klasafgeijerstam@gmail.com>
2017-04-24 20:55:10 +0200

    Create mixedfractions.cpp

---
+++ c++/mixedfractions.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    long p1,p2;
+    while (cin >> p1 >> p2 && p1 != 0 && p2 != 0)
+        cout << p1 / p2 << " " << p1%p2 << " / " << p2 << endl;
+    return 0;
+}
+
