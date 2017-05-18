342d6bfe6cf035f7bd0e392d12c60e5b2a597fa8
Klas af Geijerstam <klasafgeijerstam@gmail.com>
2017-05-18 20:33:20 +0200

    Create planina.cs

---
+++ c#/planina.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Linq;
+
+namespace Kattis
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine(Enumerable.Range(0, int.Parse(Console.ReadLine())).Aggregate(2, (x, y) => x * 2 - 1, x => x * x));
+        }
+    }
+}
