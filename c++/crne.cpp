bab9236bb3210e1ef2da7892370686721bcf4906
Klas af Geijerstam <klasafgeijerstam@gmail.com>
2017-07-09 00:15:26 +0200

    Update crne.cpp

--- c++/crne.cpp
+++ c++/crne.cpp
@@ -1,29 +1,14 @@
 #import <iostream>
 
-
-
 using namespace std;
 
-
-
 int main(){
-
     long p;
-
     cin >> p;
-
     if(p&1){
-
         cout << ((p+1)*(p+3))/4 << endl;
-
     }
-
     else
-
         cout << (p/2+1)*(p/2+1) << endl;
-
-
-
-return 0;
-
+    return 0;
 }
