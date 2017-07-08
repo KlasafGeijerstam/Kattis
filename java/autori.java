bf73070a9dd1a9d247d1218ada2f8aa5336b6771
Klas af Geijerstam <klasafgeijerstam@gmail.com>
2017-07-08 23:53:07 +0200

    Update autori.java

--- java/autori.java
+++ java/autori.java
@@ -1,17 +1,8 @@
 import java.util.*;
-
 import java.util.stream.Collectors;
 
-
-
 public class main {
-
-
-
     public static void main(String[] args) throws Exception{
-
         new ArrayList<>(new Scanner(System.in).nextLine().chars().mapToObj(e -> (char)e).collect(Collectors.toList())).forEach(x -> System.out.print(Character.isUpperCase(x) ? x : ""));
-
     }
-
 }
