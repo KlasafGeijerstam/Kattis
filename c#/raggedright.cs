09b2527441a7cee1f59a8bde138acc45eadb79dc
Klas af Geijerstam <klasafgeijerstam@gmail.com>
2017-07-08 23:39:04 +0200

    Update raggedright.cs

--- c#/raggedright.cs
+++ c#/raggedright.cs
@@ -1,59 +1,29 @@
 using System;
-
 using System.Linq;
-
 using System.Numerics;
-
 using System.Collections.Generic;
-
 using System.IO;
-
 using System.Diagnostics;
-
 using System.Globalization;
 
-
-
 namespace Kattis
-
 {
-
-
-
-    public class Program
-
+  public class Program
+  {
+    public static void Main()
     {
-
-        public static void Main()
-
-        {
-
-            string str;
-
-            var max = 0;
-
-            var l = new List<int>();
-
-            while((str = Console.ReadLine()) != null)
-
-            {
-
-               l.Add(str.Length);
-
-            }
-
-            max = l.Max();
-
-            l.RemoveAt(l.Count - 1);
-
-            int tot = 0;
-
-            l.ForEach(x => tot += (int)Math.Pow(max - x, 2));
-
-            Console.WriteLine(tot);
-
-        }
-
+      string str;
+      var max = 0;
+      var l = new List<int>();
+      while((str = Console.ReadLine()) != null)
+      {
+         l.Add(str.Length);
+      }
+      max = l.Max();
+      l.RemoveAt(l.Count - 1);
+      int tot = 0;
+      l.ForEach(x => tot += (int)Math.Pow(max - x, 2));
+      Console.WriteLine(tot);
     }
-
+  }
 }
