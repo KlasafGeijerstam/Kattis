72b873d03aaab8ffdab6a712beb3b4075d91286d
Klas af Geijerstam <klasafgeijerstam@gmail.com>
2017-07-08 23:36:22 +0200

    Update parking2.cs

--- c#/parking2.cs
+++ c#/parking2.cs
@@ -1,29 +1,16 @@
 using System;
-
-using System.Linq;
-
-                    
+using System.Linq;                  
 
 public class Program
-
 {
-
     public static void Main()
-
     {
-
         var c = int.Parse(Console.ReadLine());
-
+      
         for(int i = 0; i < c; i++){
-
             Console.ReadLine();
-
             var p = Console.ReadLine().Split(' ').Select(x => int.Parse(x));
-
             Console.WriteLine((p.Max()-p.Min())*2);
-
         }
-
     }
-
 }
