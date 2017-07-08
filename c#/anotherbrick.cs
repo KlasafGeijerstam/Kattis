b019106e5b5b17baffc67b7bbf456ac7f2f37e47
Klas af Geijerstam <klasafgeijerstam@gmail.com>
2017-07-08 23:29:59 +0200

    Update anotherbrick.cs

--- c#/anotherbrick.cs
+++ c#/anotherbrick.cs
@@ -1,65 +1,35 @@
 using System;
-
 using System.Linq;
-
 using System.Collections.Generic;       
-
 public class Program
-
 {
-
     public static void Main()
-
     {
-
         var arr = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
-
         int h = arr[0];
-
         int w = arr[1];
-
         var q = new Queue<int>();
-
+        
         Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToList().ForEach(x => q.Enqueue(x));
-
         int curw = 0;
-
         int curh = 0;
-
         bool failed = false;
-
+        
         while(curh < h){
-
             if(q.Count == 0){
-
                 failed = false;
-
                 break;
-
             }
-
             curw += q.Dequeue();
-
             if(curw == w){
-
                 curw = 0;
-
                 curh++;
-
             }
-
             else if(curw > w){
-
                 failed = true;
-
                 break;
-
             }
-
         }
-
         Console.WriteLine(failed ? "NO" : "YES");
-
     }
-
 }
