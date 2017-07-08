537da80a11500ec65928b6e9a93c071ee7e9b557
Klas af Geijerstam <klasafgeijerstam@gmail.com>
2017-07-08 23:38:03 +0200

    Update pascal.cs

--- c#/pascal.cs
+++ c#/pascal.cs
@@ -1,69 +1,34 @@
-using System;
-
-                    
+using System;                  
 
 public class Program
-
 {
-
     public static void Main()
-
     {
-
         int n = int.Parse(Console.ReadLine());
-
         if(isPri(n)){
-
             Console.WriteLine(n-1);
-
             return;
-
         }
-
-            for(int i = 2; i <= n; i++){
-
-                if(n % i == 0){
-
-                    Console.WriteLine((n/i)*(i-1));
-
-                    return;
-
-                }
-
-            }
-
+        for(int i = 2; i <= n; i++){
+          if(n % i == 0){
+            Console.WriteLine((n/i)*(i-1));
+            return;
+          }
+        }
     }
-
     static bool isPri(long x)
-
     {
-
         if(x == 2)
-
-            return true;
-
-        
-
+          return true;
         if(x % 2 == 0)
+          return false;
 
-            return false;
-
-        for(long i = 3; ; i+= 2)
-
-        {
-
-            long q = x/i;
-
-            if(q <i)
-
-                return true;
-
-            if(x == q*i)
-
-                return false;
-
+        for(long i = 3; ; i+= 2){
+          long q = x/i;
+          if(q <i)
+              return true;
+          if(x == q*i)
+              return false;
         }
-
-}
-
+    }
 }
