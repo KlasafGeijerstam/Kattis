70cc6883f8010b611109e7e62ac0e4983a51d0a5
Klas af Geijerstam <klasafgeijerstam@gmail.com>
2017-07-09 00:03:15 +0200

    Update filip.java

--- java/filip.java
+++ java/filip.java
@@ -1,17 +1,8 @@
 import java.util.*;
-
 import java.util.stream.Collectors;
 
-
-
 public class main {
-
-
-
     public static void main(String[] args) throws Exception{
-
         System.out.println(Collections.max(Arrays.stream(new Scanner(System.in).nextLine().split(" ")).map(x -> new StringBuilder(x).reverse().toString()).collect(Collectors.toList())));
-
     }
-
 }
