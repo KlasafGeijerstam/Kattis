4abdbcb976ee588f9cbce583b745be115b44c52c
Klas af Geijerstam <klasafgeijerstam@gmail.com>
2017-07-20 17:37:30 +0200

    Update cokolada.cs

--- c#/cokolada.cs
+++ c#/cokolada.cs
@@ -1,85 +1,43 @@
 using System;
-
 using System.Collections.Generic;
-
 using System.Globalization;
-
 using System.Linq;
-
 using System.Numerics;
 
-
-
 namespace Kattis
-
 {
-
     public class Program
-
     {
-
         public static void Main()
-
         {
-
             var l = uint.Parse(Console.ReadLine());
-
             if(Closest(l) == l)
-
                 Console.WriteLine(l + " " + 0);
-
             else
-
             {
-
                 int breaks = 1;
-
                 uint target = Closest(l) - l;
-
                 uint cur = Closest(l) / 2;
-
                 while(cur != target)
-
                 {
-
                     breaks++;
-
                     if (cur / 2 < target)
-
                         target -= cur / 2;
-
                     cur = cur / 2;
-
                 }
-
                 Console.WriteLine(Closest(l) + " " + breaks);
-
             }
-
             Console.ReadLine();
-
         }
-
-
 
         static uint Closest(uint p)
-
         {
-
             if (Convert.ToString(p, 2).Count(x => x == '1') > 1)
-
             {
-
                 return 2147483648 >> (Convert.ToString(p, 2).IndexOf('1') + 31 - Convert.ToString(p, 2).Length);
-
             }
-
             else
-
                 return p;
-
         }
-
     }
-
 }
