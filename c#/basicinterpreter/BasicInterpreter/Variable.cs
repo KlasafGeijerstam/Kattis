86ec48fbb5a5cb1c5a882800153acd1829c516cb
Klas af Geijerstam <klasafgeijerstam@gmail.com>
2017-07-27 19:36:09 +0200

    Create Variable.cs

---
+++ c#/basicinterpreter/BasicInterpreter/Variable.cs
@@ -0,0 +1,54 @@
+using System;
+
+namespace BasicInterpreter
+{
+    internal class Variable
+    {
+        private string identifier;
+        private object value;
+        private VariableType type;
+        private Variable()
+        {
+
+        }
+        public override string ToString()
+        {
+            return type == VariableType.Variable ? Interpreter.variables[identifier].ToString() : value.ToString();
+        }
+
+        public int Get()
+        {
+            if (type == VariableType.Variable)
+                return Interpreter.variables[identifier];
+            else if (type == VariableType.Constant)
+                return (int)value;
+            else
+                throw new NotSupportedException("Can not convert StringConstant to int");
+        }
+
+        public static Variable FromString(string i)
+        {
+            if (char.IsUpper(i[0]))
+                return new Variable()
+                {
+                    type = VariableType.Variable,
+                    identifier = i
+                };
+
+            else if (char.IsDigit(i[0]) || i[0] == '-')
+                return new Variable()
+                {
+                    type = VariableType.Constant,
+                    value = int.Parse(i)
+                };
+            else
+                return new Variable()
+                {
+                    type = VariableType.StringConstant,
+                    value = i.Replace("\"", string.Empty)
+                       
+                };
+        }
+    }
+}
+ 
