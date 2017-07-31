164f0937122d4be680bc12873ad8b28a55a8a338
Klas af Geijerstam <klasafgeijerstam@gmail.com>
2017-07-31 20:47:31 +0200

    Create jumbled.cs

---
+++ c#/jumbled.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Kattis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            n2 -= n1;
+            if (n2 > 180)
+                n2 -= 360;
+            if (n2 <= -180)
+                n2 += 360;
+            Console.WriteLine(n2);
+        }
+    }
+}
