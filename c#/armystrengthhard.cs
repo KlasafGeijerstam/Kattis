7fa6ee9f22a0fd17e245901f4a98c44d5b05a1f0
Klas af Geijerstam <klasafgeijerstam@gmail.com>
2017-08-04 18:31:10 +0200

    Update armystrengthhard.cs

--- c#/armystrengthhard.cs
+++ c#/armystrengthhard.cs
@@ -1,67 +1,30 @@
 using System;
-
 using System.Linq;
-
 using System.Numerics;
-
 using System.Collections.Generic;
-
 using System.IO;
-
 using System.Diagnostics;
 
-
-
 namespace Kattis
-
 {
-
-
-
     public class Program
-
     {
-
         public static void Main()
-
         {
-
             int cases = int.Parse(Console.ReadLine());
 
-
-
             for (int i = 0; i < cases; i++)
-
             {
-
                 Console.ReadLine();
-
-                
-
                 Console.ReadLine();
-
                 var god = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).Max();
-
                 var mec = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).Max();
 
-
-
                 if(god >= mec)
-
                     Console.WriteLine("Godzilla");
-
                 else
-
                     Console.WriteLine("MechaGodzilla");
-
-                
-
             } 
-
         }
-
-
-
     }
-
 }
