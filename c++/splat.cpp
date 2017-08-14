6bba2e3d6f948b48f550aad8b9653039a24ca395
Klas af Geijerstam <klasafgeijerstam@gmail.com>
2017-08-14 17:34:23 +0200

    Create splat.cpp

---
+++ c++/splat.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+struct Circle
+{
+    double x, y, r;
+    std::string colour;
+
+    bool hit(double px, double py)
+    {
+        return pow(px - x, 2) + pow(py - y, 2) <= r*r;
+    }
+};
+
+double rFromV(double v)
+{
+    return sqrt(v / 3.14159265359);
+}
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    int t;
+    std::cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        std::vector<Circle> circ;
+        int d;
+        double x, y, v;
+        std::string col;
+        std::cin >> d;
+        for (int dr = 0; dr < d; dr++)
+        {
+            std::cin >> x >> y >> v >> col;
+            circ.push_back({x, y, rFromV(v), col});
+        }
+        std::cin >> d;
+        for (int dr = 0; dr < d; dr++)
+        {
+            std::cin >> x >> y;
+            col = "white";
+            for (int j = circ.size() - 1; j >= 0; j--)
+            {
+                if (circ[j].hit(x, y)) {
+                    col = circ[j].colour;
+                    break;
+                }
+            }
+            std::cout << col << std::endl;
+        }
+    }
+}
