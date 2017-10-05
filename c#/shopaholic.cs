11bc6c5eeab88a9822ddc0f7e7a2f7d412d64c75
Klas af Geijerstam <klasafgeijerstam@gmail.com>
2017-10-05 09:40:22 +0200

    Update shopaholic.cs

--- c#/shopaholic.cs
+++ c#/shopaholic.cs
@@ -1,31 +1,15 @@
 using System;
-
 using System.Linq;
 
-
-
 public class Program
-
 {
-
     public static void Main()
-
     {
-
         Console.ReadLine();
-
         var l = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).OrderByDescending(x => x).ToList();
-
         var discount = 0l;
-
-        for(int i = 2; i < l.Count; i+=3){
-
+        for(int i = 2; i < l.Count; i+=3)
             discount += l[i];
-
-        }
-
         Console.WriteLine(discount);
-
     }
-
 }
