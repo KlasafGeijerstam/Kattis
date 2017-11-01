ceff088e2c8fd438a5e3049cc7f6fea5dbb88cc9
Klas af Geijerstam <klasafgeijerstam@gmail.com>
2017-11-01 16:15:42 +0100

    Update nine.cs

--- c#/nine.cs
+++ c#/nine.cs
@@ -1,33 +1,17 @@
 using System;
-
 using System.Collections.Generic;
-
 using System.Globalization;
-
 using System.Linq;
-
 using System.Text;
-
 using System.Numerics;
 
-
-
 namespace Kattis
-
 {
-
     public class Program
-
     {
-
         public static void Main()
-
         {
-
             Enumerable.Range(0, int.Parse(Console.ReadLine())).ToList().ForEach(x => Console.WriteLine((8 * BigInteger.ModPow(9, long.Parse(Console.ReadLine())-1, 1000000007)) % 1000000007));
-
         }
-
     }
-
 } 
