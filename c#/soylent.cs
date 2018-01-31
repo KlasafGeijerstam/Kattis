19aa422125fb794e3edccf5302e18b6af15b6bdb
Klas af Geijerstam <klasafgeijerstam@gmail.com>
2018-01-31 15:06:47 +0100

    Update soylent.cs

--- c#/soylent.cs
+++ c#/soylent.cs
@@ -5,8 +5,8 @@ public class Program
     public static void Main()
     {
         int i = int.Parse(Console.ReadLine());
-            for(int j = 0; j < i; j++){
-                Console.WriteLine(Math.Ceiling((int.Parse(Console.ReadLine()))/400f));  
-            }
+        for(int j = 0; j < i; j++){
+            Console.WriteLine(Math.Ceiling((int.Parse(Console.ReadLine()))/400f));  
+        }
     }
 }
