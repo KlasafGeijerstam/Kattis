765c201614dc257d626ae3a7936a58e192501e64
Klas af Geijerstam <klasafgeijerstam@gmail.com>
2018-02-03 00:51:23 +0100

    Update sjecista.cpp

--- c++/sjecista.cpp
+++ c++/sjecista.cpp
@@ -3,5 +3,5 @@
 int main() {
     int x;
     std::cin >> x;
-    std::cout << ((x == 3) ? 0 : (x*(x - 1)*(x - 2)*(x - 3))/24) << std::endl;
+    std::cout << x*(x - 1)*(x - 2)*(x - 3))/24 << std::endl;
 }
