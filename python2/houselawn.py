42a2c79fca9161a625043062cd5e85b36d03011a
Klas af Geijerstam <klasafgeijerstam@gmail.com>
2018-10-08 16:37:01 +0200

    Update houselawn.py

--- python2/houselawn.py
+++ python2/houselawn.py
@@ -6,14 +6,13 @@ if __name__ == '__main__':
     lowest = 10000000
     for i in range(m):
         a = raw_input().split(',')
-        name = a[0]
         p, c, t, r = map(float, a[1:])
         if ((c*t)/(t+r)) * 10080 >= l:
             if p < lowest:
                 lowest = p
-                names = [name]
+                names = [a[0]]
             elif p == lowest:
-                names.append(name)
+                names.append(a[0])
     if len(names) == 0:
         print("no such mower")
     else:
