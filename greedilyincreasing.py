0da4be3c4203c53707ec0daa460303250ced653b
Klas af Geijerstam <klasafgeijerstam@gmail.com>
2018-11-06 09:40:28 +0100

    Delete greedilyincreasing.py

--- greedilyincreasing.py
+++
@@ -1,9 +0,0 @@
-raw_input()
-t = [i for i in map(int, raw_input().split())]
-g = [t[0]]
-for i in range(1,len(t)):
-    if t[i] > g[-1]:
-        g.append(t[i])
-
-print len(g)
-print " ".join(map(str, g))
